f684dca2eefd1f4a996d9e68a9723992cd8ed87a
xiaohei <1104375640@qq.com>
2019-01-07 15:16:30 +0800

    update config

--- src/main/java/com/luokes/config/Config.java
+++ src/main/java/com/luokes/config/Config.java
@@ -2,9 +2,9 @@ package com.luokes.config;
 
 public class Config {
     //输入的xml文件
-    public final static String inPutXmlFile = "/Users/howard/AndroidTest/C60/app/src/main/res/values/strings.xml";
+    public final static String inPutXmlFile = "/Users/howard/AndroidTest/Test/app/src/main/res/values/strings.xml";
     //输出的xml文件
-    public final static String outPutXmlFile = "/Users/howard/AndroidTest/C60/app/src/main/res/values/test1.xml";
+    public final static String outPutXmlFile = "/Users/howard/AndroidTest/Test/app/src/main/res/values-ja-rJP/strings.xml";
     //输入的语言
     public final static String inputLanguage = "zh-CN";
     //输出的语言
